767abe688050fc515ac9d91a62d4e909ee286de8
Kelvin <kelvin.daquon.wade@gmail.com>
2023-01-30 17:40:32 -0600

    fixing api key

--- food-app/src/conponets/Popular.jsx
+++ food-app/src/conponets/Popular.jsx
@@ -7,7 +7,7 @@ function Popular(){
 
     const getPopular =async() =>{
         const api =await fetch(
-            `https://api.spooncular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`)
         const data=await api.json();
         console.log(data)
     }
