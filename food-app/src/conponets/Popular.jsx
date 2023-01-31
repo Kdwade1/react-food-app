35c354b406a688bd2942c0a4394b9e86c6069889
Kelvin <kelvin.daquon.wade@gmail.com>
2023-01-31 15:21:05 -0600

    fixed api call

--- food-app/src/conponets/Popular.jsx
+++ food-app/src/conponets/Popular.jsx
@@ -1,17 +1,33 @@
-import { useEffect } from "react";
+import {useEffect,useState} from "react";
+import styled from "styled-components";
 
-function Popular(){
-    useEffect(()=>{
+function Popular() {
+
+    const [popular,setPopular] = useState([])
+    useEffect(() => {
         getPopular();
-    },[])
+    }, [])
 
-    const getPopular =async() =>{
-        const api =await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`)
-        const data=await api.json();
-        console.log(data)
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=f3bf7a5d0429424baae3198eaed90aa2&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes)
     }
-    return(
-        <div>Popular</div>
+    return (
+        <div>
+            {popular.map((recipes) =>{
+                return(
+                    <div key={recipes.id}>
+                        <p>
+                            {recipes.title}
+                        </p>
+                    </div>
+
+                )
+            })
+            }
+        </div>
     )
 }
+
 export default Popular
